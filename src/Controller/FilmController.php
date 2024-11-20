e165d1f9ee7280caa709ea38a0af433c84c0d134
Xavier Alibert <xavier.alibert@etu-paris1.fr>
2024-11-20 16:01:32 +0100

    edit

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -4,6 +4,28 @@ namespace App\Controller;
 
 class FilmController
 {
+
+    public function list(array $queryParams){
+        
+        $filmsEntities[] = new film();
+
+        foreach($films as $film){
+            
+            $filmEntity->setId($film["id"]):
+            $filmEntity->setTitle($film["title"]):
+            $filmEntity->setYear($film["year"]):
+            $filmEntity->setType($film["type"]):
+            $filmEntity->setSynopsis($film["synopsis"]):
+            $filmEntity->setDirector(new \DateTime($film["director"])):
+            $filmEntity->setCreatedAt(new \DateTime($film["created_at"])):
+    
+            $filmsEntities[] = $filmEntity;
+        }
+    }
+
+
+
+
     public function create()
     {
         echo "Création d'un film";

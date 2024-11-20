d73973104522b972b2f84123e9adf22cb1483f91
Xavier Alibert <xavier.alibert@etu-paris1.fr>
2024-11-20 12:35:44 +0100

    edit film

--- src/Entity/Film.php
+++ src/Entity/Film.php
@@ -4,5 +4,20 @@ namespace App\Film;
 
     class Film{
         
+        private int $id;	
+        private string $title;	
+        private ?int $year = null;
+        private string $genre;	
+        private string $synopsis;	
+        private string $director;	
+        private ?\DateTime $deleted_at = null;
+        private \DateTime $created_at;
+        private ?\DateTime $updated_at = null;
+
+        
+
+
+
+
     }
 ?>
